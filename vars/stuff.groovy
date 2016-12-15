f2b83b352b3307d6144198313cfe3a453be9e5b8
Joseph Wright <rjosephwright@gmail.com>
2016-12-15 07:54:52 -0800

    Really use JsonSlurperClassic

--- vars/stuff.groovy
+++ vars/stuff.groovy
@@ -3,5 +3,5 @@ import groovy.json.JsonSlurperClassic
 someVar = 'This is a string'
 
 def getJson(url) {
-    new JsonSlurper().parseText(new URL(url).text)
+    new JsonSlurperClassic().parseText(new URL(url).text)
 }
